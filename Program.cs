46d8e0bcf20d83fe76ddcbd237b475c14b5fca74
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-25 22:18:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace project_mina_database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
