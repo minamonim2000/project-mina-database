7f15223052970c600482bbd8873e378cf352796b
Mina <Mina@DESKTOP-OKJR5TV>
2023-01-25 23:53:07 +0200

    using Collections.Generic

--- Program.cs
+++ Program.cs
@@ -1,10 +1,2 @@
-﻿namespace project_mina_database
-{
-    internal class Program
-    {51
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}tggfjvnh
+﻿using System;
+using System.Collections.Generic;
